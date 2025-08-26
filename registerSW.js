dfa74b22df66fd952033c1e6062e21e9ddef80d0
kkk-kppi <79731962+kkk-kppi@users.noreply.github.com>
2025-08-26 02:20:23 +0000

    Deploying to gh-pages from @ kkk-kppi/vue3-web-admin@70bef8addb2fca7dac0ee514a79e589e6e6efed3 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/v3-admin/sw.js', { scope: '/v3-admin/' })})}
